97efb0bfe0e30741f3fd84d4f1043d18e459209f
Madyson Ward <MadysonWard94@gmail.com>
2017-10-03 15:44:52 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Header from "./Header";
+import './index.css';
+
+ReactDOM.render(
+	<div className="row">
+		<App>
+			<Header />
+		</App>
+	</div>,
+	document.getElementById('root')
+);
